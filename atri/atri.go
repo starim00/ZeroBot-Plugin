29ab0a27de2fed53f2cad7d436f8e861989658cc
hutiance <hutiance@newings.net.cn>
2021-05-31 12:38:47 +0800

    让老婆不要那么高冷

--- atri/atri.go
+++ atri/atri.go
@@ -39,9 +39,9 @@ func init() { // 插件主体
 		})
 	zero.OnKeywordGroup([]string{"喜欢", "爱你", "爱", "suki", "daisuki", "すき", "好き", "贴贴", "老婆", "亲一个", "mua"}, AtriSwitch(), AtriSleep(), zero.OnlyToMe).SetBlock(true).SetPriority(PRIO).
 		Handle(func(ctx *zero.Ctx) {
-			if rand.Intn(10) == 0 {
-				ctx.SendChain(randImage("SUKI.jpg", "SUKI1.jpg", "SUKI2.png"))
-			}
+			//if rand.Intn(10) == 0 {
+			ctx.SendChain(randImage("SUKI.jpg", "SUKI1.jpg", "SUKI2.png"))
+			//}
 		})
 	zero.OnKeywordGroup([]string{"草你妈", "操你妈", "脑瘫", "废柴", "fw", "five", "废物", "战斗", "爬", "爪巴", "sb", "SB", "傻B"}, AtriSwitch(), AtriSleep(), zero.OnlyToMe).SetBlock(true).SetPriority(PRIO - 1).
 		Handle(func(ctx *zero.Ctx) {
