e4e101835f875e8dc698e34d70bf225d1c7dab38
hutiance <hutiance@newings.net.cn>
2022-01-11 18:19:41 +0800

    设置token

--- main.go
+++ main.go
@@ -100,9 +100,9 @@ func init() {
 	// g := flag.String("g", "127.0.0.1:3000", "Set web gui listening address.")
 
 	// 直接写死 AccessToken 时，请更改下面第二个参数
-	token = flag.String("t", "", "Set AccessToken of WSClient.")
+	token = flag.String("t", "xiaohanhan", "Set AccessToken of WSClient.")
 	// 直接写死 URL 时，请更改下面第二个参数
-	url = flag.String("u", "ws://127.0.0.1:6700", "xiaohanhan")
+	url = flag.String("u", "ws://127.0.0.1:6700", "Set Url of WSClient.")
 	// 默认昵称
 	adana = flag.String("n", "寿司", "Set default nickname.")
 	prefix = flag.String("p", "/", "Set command prefix.")
